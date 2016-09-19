ae0e5b505bdb183f72ee5b4d36697aa0333f9075
Feroz Ahmmed <froghramar@gmail.com>
2016-09-19 18:21:49 +0600

    32nd

---
+++ 740 - Baudot Data Communication Code.cpp
@@ -0,0 +1,48 @@
+/******************************************************************************
+*  ▄██████████▄  ▀█████████▄   ▀██████████▄    ▄████████████▄   ▄██████████▄  *
+* ▀▀███    ███▀    ███    ███    ███     ▀██▄  ▀███▀    ▀███▀   █▀    ▄███▀   *
+*   ███            ███           ███      ██    ███      ███         ▄███▀    *
+*  ▄███▄▄▄██▀    ▄███▄▄▄██▀      ███      ██▀   ███      ███        ▄███▀     *
+* ▀▀███▀▀▀██▄   ▀▀███▀▀▀██▄     ▄███▄▄▄▄▄██▀    ███      ███       ▄███▀      *
+*   ███            ███         ▀▀███▀▀▀▀▀██▄    ███      ███      ▄███▀       *
+*   ███            ███    ███    ███     ███   ▄███▄    ▄███▄    ▄███▀    ▄█  *
+* ▄▄████▀        ▄█████████▀    ▄███     ███▄  ▀████████████▀   ▀██████████▀  *
+*                                                                             *
+*                        *Don't limit your challenges, challenge your limits. *
+******************************************************************************/
+
+#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 1000005;
+
+char l[2][33], s[100];
+bool shift;
+int mask;
+
+void init(){
+    gets(l[0]);
+    gets(l[1]);
+    while(scanf("%s",s) == 1){
+        shift = 0;
+        for(int i = 0; s[i] ; i++){
+            mask = mask * 2 + (s[i] - '0');
+            if((i + 1) % 5) continue;
+            if(mask == 27) shift = 0;
+            else if(mask == 31) shift = 1;
+            else putchar(l[shift][mask]);
+            mask = 0;
+        }
+        puts("");
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    init();
+    return 0;
+}
