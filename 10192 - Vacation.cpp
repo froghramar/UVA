4491017ddf7f33cd840ff96f8f69d5e741f5c10f
Feroz Ahmmed <froghramar@gmail.com>
2016-09-19 18:29:04 +0600

    33rd

---
+++ 10192 - Vacation.cpp
@@ -0,0 +1,46 @@
+/******************************************************************************
+*  ▄██████████▄  ▀█████████▄   ▀██████████▄    ▄████████████▄   ▄██████████▄  *
+* ▀▀███    ███▀    ███    ███    ███     ▀██▄  ▀███▀    ▀███▀   █▀    ▄███▀   *
+*   ███            ███           ███      ██    ███      ███         ▄███▀    *
+*  ▄███▄▄▄██▀    ▄███▄▄▄██▀      ███      ██▀   ███      ███        ▄███▀     *
+* ▀▀███▀▀▀██▄   ▀▀███▀▀▀██▄     ▄███▄▄▄▄▄██▀    ███      ███       ▄███▀      *
+*   ███            ███         ▀▀███▀▀▀▀▀██▄    ███      ███      ▄███▀       *
+*   ███            ███    ███    ███     ███   ▄███▄    ▄███▄    ▄███▀    ▄█  *
+* ▄▄████▀        ▄█████████▀    ▄███     ███▄  ▀████████████▀   ▀██████████▀  *
+*                                                                             *
+*                        *Don't limit your challenges, challenge your limits. *
+******************************************************************************/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+const int N = 105;
+
+char a[N], b[N];
+int t, l, r, dp[N][N];
+
+void init(){
+    while(1){
+        gets(a);
+        if(strcmp(a,"#")==0) break;
+        gets(b);
+        l = strlen(a);
+        r = strlen(b);
+        for(int i = 1; i <= l; i++){
+            for(int j = 1; j <= r; j++){
+                if(a[i - 1] == b[j - 1]) dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        printf("Case #%d: you can visit at most %d cities.\n",++t, dp[l][r]);
+    }
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt","r",stdin);
+        freopen("output.txt","w",stdout);
+    #endif
+    init();
+    return 0;
+}
